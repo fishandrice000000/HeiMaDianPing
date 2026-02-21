7198dff1c05103dc5be0b4c7122d09ff14b529ce
fishandrice <qimingfeialways@163.com>
2026-02-21 23:13:59 +0800

    Interceptor: 解耦登录校验和Token刷新

--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,33 +1,17 @@
 package com.hmdp.utils;
 
-import cn.hutool.core.bean.BeanUtil;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.hmdp.dto.UserDTO;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
-import static com.hmdp.utils.RedisConstants.LOGIN_USER_KEY;
-import static com.hmdp.utils.RedisConstants.LOGIN_USER_TTL;
 
 /**
  * 登录拦截器
  */
 public class LoginInterceptor implements HandlerInterceptor {
-    private StringRedisTemplate stringRedisTemplate;
-
-    public LoginInterceptor(StringRedisTemplate stringRedisTemplate) {
-        this.stringRedisTemplate = stringRedisTemplate;
-    }
-
     /**
-     * 校验登录状态并刷新Token时效
+     * 校验登录状态
      *
      * @param request  current HTTP request
      * @param response current HTTP response
@@ -35,42 +19,18 @@ public class LoginInterceptor implements HandlerInterceptor {
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        // 从请求头中获取token
-        String token = request.getHeader("authorization");
-        if (StringUtils.isBlank(token)) {
-            // 不存在则返回状态码: 401
-            response.setStatus(401);
-            // 拦截
-            return false;
-        }
-
-        // 根据token从Redis中查询user
-        String tokenKey = LOGIN_USER_KEY + token;
-        Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(tokenKey);
+        // 从ThreadLocal中获取用户
+        UserDTO userDTO = UserHolder.getUser();
 
         // 判断User是否存在
-        if (userMap.isEmpty()) {
+        if (userDTO == null) {
             // 不存在则返回状态码: 401
             response.setStatus(401);
             // 拦截
             return false;
         }
-
-        // 将用户信息保存至ThreadLocal
-        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
-        UserHolder.saveUser((UserDTO) userDTO);
-        // 更新有效期
-        stringRedisTemplate.expire(tokenKey, LOGIN_USER_TTL, TimeUnit.SECONDS);
 
         // 放行
         return true;
     }
-
-
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) {
-        // 登录验证完毕后, 释放ThreadLocal<UserDTO>
-        UserHolder.removeUser();
-    }
-
 }
