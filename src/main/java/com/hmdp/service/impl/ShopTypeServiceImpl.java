7a1a7661d74d0dac53f8e50ffe37988c8378581f
fishandrice <qimingfeialways@163.com>
2026-02-23 15:29:18 +0800

    ShopType: 实现了借助Redis缓存查询商铺类型列表的功能

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,14 +1,22 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_KEY;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -17,4 +25,37 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private ShopTypeMapper shopTypeMapper;
+
+    /**
+     * 查询商店类型列表, 利用了Redis缓存
+     */
+    @Override
+    public Result queryTypeList() {
+        // 1. 从Redis中查询列表
+        String cacheTypeKey = CACHE_SHOP_TYPE_KEY;
+        String cacheTypeListString = stringRedisTemplate.opsForValue().get(cacheTypeKey);
+
+        // 2. 若查询到, 将其转化为List, 返回
+        if (cacheTypeListString != null) {
+            List<ShopType> cacheTypeList = JSONUtil.toList(cacheTypeListString, ShopType.class);
+            return Result.ok(cacheTypeList);
+        }
+
+        // 3. 若未查询到, 去MySQL中查询列表
+        List<ShopType> typeList = query().orderByAsc("sort").list();
+
+        // 4. 若仍未查询到, 则返回错误
+        if (typeList.isEmpty()) {
+            return Result.fail("商店类型不存在");
+        }
+
+        // 5. 若查询到, 将列表写入Redis, 再返回
+        String typeJsonString = JSONUtil.toJsonStr(typeList);
+        stringRedisTemplate.opsForValue().set(cacheTypeKey, typeJsonString);
+        return Result.ok(typeList);
+    }
 }
