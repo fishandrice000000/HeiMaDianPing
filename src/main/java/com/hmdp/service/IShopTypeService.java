7ac8bad81702878bef59d00958d0a1ae2b58dd65
fishandrice <qimingfeialways@163.com>
2026-02-23 15:29:18 +0800

    ShopType: 实现了借助Redis缓存查询商铺类型列表的功能

--- src/main/java/com/hmdp/service/IShopTypeService.java
+++ src/main/java/com/hmdp/service/IShopTypeService.java
@@ -1,11 +1,12 @@
 package com.hmdp.service;
 
-import com.hmdp.entity.ShopType;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.ShopType;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author 虎哥
@@ -13,4 +14,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IShopTypeService extends IService<ShopType> {
 
+    Result queryTypeList();
 }
