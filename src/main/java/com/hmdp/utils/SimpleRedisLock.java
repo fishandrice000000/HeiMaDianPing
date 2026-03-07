4c691c1c86d00d32a7ce206cfd342975aad1dc17
fishandrice <qimingfeialways@163.com>
2026-03-07 14:21:08 +0800

    SimpleRedisLock: 改进了分布式锁, 使用Lua脚本使得释放锁行为原子化

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.hmdp.utils;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
 import cn.hutool.core.lang.UUID;
 
@@ -20,6 +23,14 @@ public class SimpleRedisLock implements ILock {
     // 随机ID, 加上线程ID, 保证每个线程创造的锁都是唯一的, 避免误删锁
     private static final String ID_PREFIX = UUID.randomUUID().toString() + "-";
 
+    // 执行释放锁操作的Lua脚本
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("scripts/unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+
     @Override
     public boolean tryLock(long timeoutSec) {
         // 获得全局线程唯一ID
@@ -32,17 +43,11 @@ public class SimpleRedisLock implements ILock {
 
     @Override
     public void unlock() {
-        // 首先获取线程的id
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-
-        /* ↓↓↓ 释放锁操作并非原子化 ↓↓↓ */
-
-        // 获取锁中的id, 检查是否一致
-        String lockId = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        // id一致才释放锁, 避免误删
-        if (threadId.equals(lockId)) {
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
+        /* ↓↓↓ 基于Lua脚本的原子化的锁的释放 ↓↓↓ */
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
     }
 
 }
